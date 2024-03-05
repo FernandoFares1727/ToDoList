ff06399931acb306fa100e1bb23253fee4197e21
FernandoFares1727 <fernandimfares@gmail.com>
2024-03-05 20:13:59 -0300

    Create save flow

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 const taskCardFolder = './tasksCard/';
 const taskCardPage = 'index.html';
 
+loadSavedTaskItens();
+
+const taskItemChanges = {
+    checkbox : 'isCompleted',
+    description: 'taskDescription'
+};
+
+function loadSavedTaskItens()
+{
+    var tasksBody = document.querySelector('#tasksBody');
+    var totalTasks = localStorage.length;
+    var completedTasks = 0;
+
+    for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        const value = localStorage.getItem(key);
+
+        const parsedValue = JSON.parse(value);
+        const taskDescription = parsedValue.taskDescription;
+        const dataid = parsedValue.dataid;
+        const isCompleted = parsedValue.isCompleted;
+
+        if (isCompleted)
+            ++completedTasks;
+
+        addTaskItem(tasksBody, taskDescription, dataid, isCompleted);
+      }
+
+    var createdTasksCounter = document.querySelector('#createdTasks').querySelector('.counter');
+    createdTasksCounter.textContent = totalTasks;
+
+    var allTasksCounter = document.querySelector('#allTasks').querySelector('.counter');
+    var completedTasksText = completedTasks + " de " + totalTasks;
+
+    allTasksCounter.textContent = completedTasksText;
+}
+
 function addTask(element)
 {
     var addTaskDiv = element.parentNode;
@@ -16,8 +53,9 @@ function addTask(element)
     var pageBody = addTaskDiv.parentNode.parentNode;
     var tasksInfo = pageBody.querySelector('#infoTasks');
     var tasksBody = pageBody.querySelector('#tasksBody');
+    var dataid = getUniqueId();
 
-    addTaskItem(tasksBody, inputValue).
+    addTaskItem(tasksBody, inputValue, dataid).
     then()
     .catch(error => {
         return;
@@ -28,22 +66,34 @@ function addTask(element)
     updateTaskInfo(tasksBody, tasksInfo);
 }
 
-function addTaskItem(tasksBody, inputValue) {
+function addTaskItem(tasksBody, inputValue, dataid, isChecked = false) {
     return new Promise((resolve, reject) => {
         // carrega o elemento 
         fetch(taskCardFolder + taskCardPage)
         .then(response => response.text())
         .then(html => {
-        
             var taskItem = document.createElement('li');
             taskItem.innerHTML = html;
         
             taskItem.classList.add('taskItem');
             var taskText = taskItem.querySelector('.taskDescription');
+
+            taskText.addEventListener('input', function() {
+                // callBack added
+                updateTaskItemChanges(dataid, taskItemChanges.description, taskText.textContent);
+            });
+
             taskText.textContent = inputValue;
+
+            if(isChecked)
+                taskText.classList.add('taskCheckboxChecked');
         
             var taskCheckbox = taskItem.querySelector('.taskCheckbox');
             taskCheckbox.addEventListener("change", taskCheckboxChanged);
+            taskCheckbox.checked = isChecked;
+
+            taskItem.setAttribute('dataid', dataid);
+            saveTaskItemChanges(taskItem);
 
             tasksBody.appendChild(taskItem);
             resolve();
@@ -60,7 +110,11 @@ function taskCheckboxChanged(event)
     var taskItem = taskCheckbox.parentNode;
     var taskDescription = taskItem.querySelector('.taskDescription');
 
-    if (taskCheckbox.checked)
+    console.log(taskItem);
+
+    var isChecked = taskCheckbox.checked;
+
+    if (isChecked)
     {
         taskDescription.classList.add("taskCheckboxChecked");
     }
@@ -73,11 +127,15 @@ function taskCheckboxChanged(event)
     var tasksInfo = tasksBody.parentNode.querySelector('#infoTasks');
     
     updateTaskInfo(tasksBody, tasksInfo, false);
+
+    var taskDataId = taskItem.getAttribute('dataid');
+    updateTaskItemChanges(taskDataId, taskItemChanges.checkbox, isChecked);
 }
 
 function updateTaskInfo(tasksBody, tasksInfo, realoded = true)
 {
     var tasksItens = tasksBody.querySelectorAll('.taskItem');
+    console.log(tasksItens);
     var taskItensCount = realoded ? tasksItens.length + 1 : tasksItens.length;
 
     var createdTasksCounter = tasksInfo.querySelector('#createdTasks').querySelector('.counter');
@@ -107,5 +165,43 @@ function deleteTaskIem(element)
     tasksBody.removeChild(taskItem);
     
     var tasksInfo = document.querySelector('#infoTasks');
-    updateTaskInfo(tasksBody, tasksInfo, false);  
+    updateTaskInfo(tasksBody, tasksInfo, false);
+    deleteTaskItemChanges(taskItem);
+}
+
+function getUniqueId()
+{
+    return Math.random().toString(16).slice(2);
+}
+
+function saveTaskItemChanges(taskItem)
+{
+    var taskDataid = taskItem.getAttribute('dataid');
+    var taskDescription = taskItem.querySelector('.taskDescription').textContent;
+    var isTaskItemCompleted = taskItem.querySelector('.taskCheckbox').checked;
+
+    var json = JSON.stringify({
+        dataid : taskDataid,
+        taskDescription : taskDescription,
+        isCompleted : isTaskItemCompleted
+    });
+
+    localStorage.setItem(taskDataid, json);
+}
+
+function updateTaskItemChanges(dataid, change, value)
+{
+    var taskItem = localStorage.getItem(dataid);
+    var parsedTaskItem = JSON.parse(taskItem);
+
+    parsedTaskItem[change] = value;
+
+    var jsonParsedTaskItem = JSON.stringify(parsedTaskItem);
+    localStorage.setItem(dataid, jsonParsedTaskItem);
+}
+
+function deleteTaskItemChanges(taskItem)
+{
+    var dataid = taskItem.getAttribute('dataid');
+    localStorage.removeItem(dataid);
 }
